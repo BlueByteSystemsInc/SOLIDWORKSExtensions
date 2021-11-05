75ffd180cce4aa8cb9c0a601200f7bb242697743
Amen Jlili (Home) <amen@bluebyte.biz>
2021-11-04 17:26:24 -0700

    Added method to interface

--- ISOLIDWORKSInstanceManager.cs
+++ ISOLIDWORKSInstanceManager.cs
@@ -10,6 +10,9 @@ namespace BlueByte.SOLIDWORKS.Extensions
         
         SldWorks GetSOLIDWORKSInstanceFromProcessID(int PID);
         SldWorks GetNewInstance(string commandLineParameters ="", int timeout = 30);
+
+
+        SldWorks GetNewInstance(string commandlineParameters = "", int timeout = 30, bool unloadaddins = false, int waittimeForAddInsToLoadInSeconds = 30);
         void ReleaseInstance(SldWorks swApp);
         void RestartInstance(ref SldWorks swApp, string commandLineParameters = "", int timeout = 30, int attempts = 5);
     }
